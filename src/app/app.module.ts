43e9f62d0ffaa2090d99dc4fb9f779d1b053b85d
Marcos Ortiz <marcos.israel1991@gmail.com>
2023-03-08 13:53:27 -0500

    Segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador-app.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/heores.module';
+import { ContadorModule } from './contador/contador/contador.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
+   
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/heores.module.ts
@@ -0,0 +1,24 @@
+import { HeroeComponent } from './heroe/heroe.component';
+import { ListadoComponent } from './listado/listado.component';
+import { NgModule }from '@angular/core'
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [
+        HeroeComponent,
+        ListadoComponent
+    ],
+    exports: [
+        HeroeComponent,
+        ListadoComponent
+    ],
+    imports: [
+        
+        CommonModule
+    ]
+
+})
+
+export class HeroesModule{
+
+}
